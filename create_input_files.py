043ec93811feda5d01f06cfc5765c1136c060c8a
HamidAbbasi-R <106018114+HamidAbbasi-R@users.noreply.github.com>
2023-07-03 14:10:46 +0100

    new input files

--- create_input_files.py
+++ create_input_files.py
@@ -1,19 +1,19 @@
 import json
 
-id_start = 56
-params = {
-    # "pH2_inlet": [0.95] * 5,
-    # "average_diameter": [0.5e-6] * 5,
-    "Vel_b": [0.001, 0.01, 0.1, 0.2, 0.5],
-    # "length_X": [30e-6] * 5,
-    # "length_YZ": [1e-6] * 5,
-    # "lattice_geometry": [True] * 5,
-    # "vf_pores": [0.3] * 5,
-    # "vf_Ni": [0.35] * 5,
-    # "vf_YSZ": [0.3] * 5,
-}
+id_start = 61
 
-n_cases = len(list(params.values())[0])
+n_cases = 50
+params = {
+    # "pH2_inlet": [0.95] * n_cases,
+    "average_diameter": [0.5e-6] * (n_cases//2) + [1e-6] * (n_cases//2),
+    "Vel_b": [0.001, 0.01, 0.1, 0.2, 0.5] * (n_cases//5),
+    "length_X": [10e-6]*5 + [20e-6]*5 + [40e-6]*5 + [50e-6]*5 + [100e-6]*5 + [10e-6]*5 + [20e-6]*5 + [30e-6]*5 + [50e-6]*5 + [100e-6]*5,
+    # "length_YZ": [10e-6] * n_cases,
+    # "lattice_geometry": [False] * n_cases,
+    # "vf_pores": [0.44] * n_cases,
+    # "vf_Ni": [0.28] * n_cases,
+    # "vf_YSZ": [0.28] * n_cases,
+}
 
 f = open('input files/inputs_template.json')
 inputs = json.load(f)
@@ -26,8 +26,9 @@ for i in range(n_cases):
     # inputs['boundary_conditions']['pH2_inlet'] = params['pH2_inlet'][i]
     # inputs['boundary_conditions']['pH2_b'] = params['pH2_inlet'][i]
     inputs['boundary_conditions']['Vel_b'] = params['Vel_b'][i]
-    # inputs['microstructure']['average_diameter'] = params['average_diameter'][i]
-    # inputs['microstructure']['length']['X'] = params['length_X'][i]
+    inputs['microstructure']['average_diameter'] = params['average_diameter'][i]
+    # inputs['microstructure']['lattice_geometry']['flag'] = params['lattice_geometry'][i]
+    inputs['microstructure']['length']['X'] = params['length_X'][i]
     # inputs['microstructure']['length']['Y'] = params['length_YZ'][i]
     # inputs['microstructure']['length']['Z'] = params['length_YZ'][i]
     inputs['file_options']['id'] = id_num
