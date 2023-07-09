613533e4b9a2f830098784d7e3e8237bd4f602f9
HamidAbbasi-R <106018114+HamidAbbasi-R@users.noreply.github.com>
2023-07-09 19:35:10 +0100

    new input files

--- create_input_files.py
+++ create_input_files.py
@@ -1,13 +1,14 @@
 import json
 
-id_start = 61
+id_start = 111
+n_cases = 4
 
-n_cases = 50
 params = {
     # "pH2_inlet": [0.95] * n_cases,
-    "average_diameter": [0.5e-6] * (n_cases//2) + [1e-6] * (n_cases//2),
-    "Vel_b": [0.001, 0.01, 0.1, 0.2, 0.5] * (n_cases//5),
-    "length_X": [10e-6]*5 + [20e-6]*5 + [40e-6]*5 + [50e-6]*5 + [100e-6]*5 + [10e-6]*5 + [20e-6]*5 + [30e-6]*5 + [40e-6]*5 + [100e-6]*5,
+    # "average_diameter": [0.5e-6] * (n_cases//2) + [1e-6] * (n_cases//2),
+    # "Vel_b": [0.001, 0.01, 0.1, 0.2, 0.5] * (n_cases//5),
+    # "length_X": [10e-6]*5 + [20e-6]*5 + [40e-6]*5 + [50e-6]*5 + [100e-6]*5 + [10e-6]*5 + [20e-6]*5 + [30e-6]*5 + [40e-6]*5 + [100e-6]*5,
+    "scale_factor": [1,2,4,6],
     # "length_YZ": [10e-6] * n_cases,
     # "lattice_geometry": [False] * n_cases,
     # "vf_pores": [0.44] * n_cases,
@@ -25,10 +26,10 @@ for i in range(n_cases):
 
     # inputs['boundary_conditions']['pH2_inlet'] = params['pH2_inlet'][i]
     # inputs['boundary_conditions']['pH2_b'] = params['pH2_inlet'][i]
-    inputs['boundary_conditions']['Vel_b'] = params['Vel_b'][i]
-    inputs['microstructure']['average_diameter'] = params['average_diameter'][i]
+    # inputs['boundary_conditions']['Vel_b'] = params['Vel_b'][i]
+    inputs['microstructure']['scale_factor'] = params['scale_factor'][i]
     # inputs['microstructure']['lattice_geometry']['flag'] = params['lattice_geometry'][i]
-    inputs['microstructure']['length']['X'] = params['length_X'][i]
+    # inputs['microstructure']['length']['X'] = params['length_X'][i]
     # inputs['microstructure']['length']['Y'] = params['length_YZ'][i]
     # inputs['microstructure']['length']['Z'] = params['length_YZ'][i]
     inputs['file_options']['id'] = id_num
