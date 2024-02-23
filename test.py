dc9b397d0b1d32732a9e5ba1814e1284e322a1c8
HamidAbbasi-R <106018114+HamidAbbasi-R@users.noreply.github.com>
2024-02-23 15:53:09 +0000

    changing test.py to run entire cell simulations

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-#%%
-from individual_systems import solve_individual_systems as sis
-if __name__ == '__main__':
-    sis(999)
+# from main import solve_anode as sis
+# if __name__ == '__main__':
+#     sis(999)
+
+from main import solve_entire_domain as ed
+if __name__=='__main__':
+    ed(2)
